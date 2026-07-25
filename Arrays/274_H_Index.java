c2a6866fbb559e130a4351cc80393bf1a21cc4a6
Nupur Walwadkar <walwadkarnupur@gmail.com>
2026-07-25 20:31:28 +0530

    Implement hIndex calculation in 274_H_Index.java

---
+++ Arrays/274_H_Index.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        for (int i = 0;i < citations.length; i++)
+        {
+            int h= citations.length - i;
+            if(citations[i] >= h)
+            {
+                return h;
+            }
+        }
+        return 0;
+        
+    }
+}
